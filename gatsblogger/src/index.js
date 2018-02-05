d258bb0ec5a88647a50c1132bc467c6b834e6f05
kunduin <15852158126@163.com>
2018-02-05 21:03:43 +0800

    update production

--- gatsblogger/src/index.js
+++ gatsblogger/src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import logo from './logo.svg';
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const start = (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+class m extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={showWarning:true}
+    }
+}
+
+function Starter(props){
+    return start;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Starter />, document.getElementById("root"));
+
 registerServiceWorker();
